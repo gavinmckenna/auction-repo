9b2a1d8426cdcf249b3e6b0d5e4c33d023f98108
gavinmckenna <gpm49@miami.edu>
2024-04-25 16:54:40 -0400

    Testing DB

--- public/js/login.js
+++ public/js/login.js
@@ -1,17 +1,33 @@
+var db = firebase.firestore();
+
 document.getElementById('login-form').addEventListener('submit', function(event) {
     event.preventDefault();
+
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    firebase.auth().signInWithEmailAndPassword(username, password)
-    .then((userCredential) => {
-
-        var user = userCredential.user;
-        window.location.href = '/dashboard.html';
+    // Proceed with Firestore operations
+    db.collection('User').where('username', '==', username).get()
+    .then(snapshot => {
+        if (snapshot.empty) {
+            throw new Error('No matching user');
+        }
+        let userAuthenticated = false;
+        snapshot.forEach(doc => {
+            const userData = doc.data();
+            // IMPORTANT: This is insecure; see the security note below
+            if (userData.password === password) {
+                userAuthenticated = true;
+                console.log('User authenticated:', userData);
+                // Proceed with user session establishment and/or redirection
+            }
+        });
+        if (!userAuthenticated) {
+            throw new Error('Incorrect username or password');
+        }
     })
-    .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        document.getElementById('login-error').textContent = 'Error: ' + errorMessage;
+    .catch(error => {
+        console.error('Authentication error:', error);
+        document.getElementById('login-error').textContent = error.message;
     });
 });
